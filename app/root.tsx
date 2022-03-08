477ab0769a4c0df42f1445901238b73c0bb049fe
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-03-08 18:46:38 +0530

    (fix): fixed scroll padding issue on mobile

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,10 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en" className="scroll-smooth scroll-p-[90px]">
+    <html
+      lang="en"
+      className="scroll-smooth scroll-p-[128px] lg:scroll-p-[90px]"
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
